7ebd6e75c75e69de1a56b87f5f1e3e13cb70da4f
mareek <mareek@gmail.com>
2013-07-10 17:20:18 +0200

    First commit with ongoing code
    I'm not even sure it compiles

---
+++ BFPlayground/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BFPlayground
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private string ExecuteBF(string program)
+        {
+            var data = new List<byte> { 0 };
+            int cursor = 0;
+
+            Action MoveCursorRight = () =>
+            {
+                if (cursor == data.Count - 1)
+                    data.Add(0);
+                cursor++;
+            };
+
+            Action MoveCursorLeft = () =>
+            {
+                if (cursor > 0)
+                    cursor++;
+            };
+
+            Action Increment = () => data[cursor] += 1;
+            Action Decrement = () => data[cursor] -= 1;
+
+            var output = new StringBuilder();
+
+            return output.ToString();
+        }
+    }
+}
