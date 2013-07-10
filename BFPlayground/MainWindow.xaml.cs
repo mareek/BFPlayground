e9741cda33eea057cd4928c4df0f35fb323e19b6
mareek <mareek@github.com>
2013-07-10 19:36:41 +0200

    Almost complete implementation
    Entièrement développé dans le train entre Macon et Lyon !

--- BFPlayground/MainWindow.xaml.cs
+++ BFPlayground/MainWindow.xaml.cs
@@ -22,37 +22,112 @@ namespace BFPlayground
         public MainWindow()
         {
             InitializeComponent();
+            this.CodeTextBox.Text = "[-]>[-]<>+++++++[<+++++++>-]<+++.--.";
         }
 
         private void GoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                MessageBox.Show(this, ExecuteBF(this.CodeTextBox.Text), "Output");
+            }
+            catch
+            {
+                MessageBox.Show(this, "ERROR", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private string ExecuteBF(string program)
         {
+            var instructions = "<>+-[].,";
+            var code = program.Where(c => instructions.Contains(c)).ToArray();
+            var codePointer = 0;
+            var lastOpeningBracket = -1;
+
             var data = new List<byte> { 0 };
-            int cursor = 0;
+            int dataPointer = 0;
+
+            var output = new List<byte>();
 
             Action MoveCursorRight = () =>
             {
-                if (cursor == data.Count - 1)
+                if (dataPointer == data.Count - 1)
                     data.Add(0);
-                cursor++;
+                dataPointer++;
             };
 
             Action MoveCursorLeft = () =>
             {
-                if (cursor > 0)
-                    cursor++;
+                if (dataPointer == 0)
+                    throw new ApplicationException("Moved pointer before the beggining of the data");
+                else
+                    dataPointer--;
             };
 
-            Action Increment = () => data[cursor] += 1;
-            Action Decrement = () => data[cursor] -= 1;
+            Action Increment = () => data[dataPointer] += 1;
 
-            var output = new StringBuilder();
+            Action Decrement = () => data[dataPointer] -= 1;
 
-            return output.ToString();
+            Action OpeningBracket = () =>
+            {
+                lastOpeningBracket = codePointer;
+                if (data[dataPointer] == 0)
+                {
+                    while (codePointer < code.Length && code[codePointer] != ']')
+                        codePointer++;
+                    if (codePointer == code.Length)
+                    {
+                        throw new ApplicationException("No matching closing bracket");
+                    }
+                }
+            };
+
+            Action ClosingBracket = () =>
+            {
+                if (data[dataPointer] == 0)
+                { /*Do nothing*/ }
+                else if (lastOpeningBracket >= 0)
+                    codePointer = lastOpeningBracket;
+                else
+                    throw new ApplicationException("No matching opening bracket");
+            };
+
+            Action Input = () => { };
+            Action Output = () => output.Add(data[dataPointer]);
+
+            while (codePointer < code.Length)
+            {
+                switch (code[codePointer])
+                {
+                    case '>':
+                        MoveCursorRight();
+                        break;
+                    case '<':
+                        MoveCursorLeft();
+                        break;
+                    case '+':
+                        Increment();
+                        break;
+                    case '-':
+                        Decrement();
+                        break;
+                    case '[':
+                        OpeningBracket();
+                        break;
+                    case ']':
+                        ClosingBracket();
+                        break;
+                    case ',':
+                        Input();
+                        break;
+                    case '.':
+                        Output();
+                        break;
+                }
+                codePointer++;
+            }
+
+            return new string(UTF8Encoding.UTF8.GetChars(output.ToArray()));
         }
     }
 }
