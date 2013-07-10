0f89dcebf09b9b920d2a8612d8ffe343ba8fba01
mareek <mareek@gmail.com>
2013-07-10 20:22:24 +0200

    Move the interpreter to his own class and made it more objecty

---
+++ BFPlayground/BrainFuckInterpreter.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BFPlayground
+{
+    class BrainFuckInterpreter
+    {
+        private const string instructions = "<>+-[].,";
+
+        private string _program;
+
+        private char[] _code;
+        private int _codePointer = 0;
+
+        private List<byte> _data = new List<byte> { 0 };
+        int _dataPointer = 0;
+
+        List<byte> _output;
+
+        int _lastOpeningBracket = -1;
+
+        public BrainFuckInterpreter(string program)
+        {
+            _program = program;
+            _code = _program.Where(c => instructions.Contains(c)).ToArray();
+        }
+
+        public string Run()
+        {
+            _output = new List<byte>();
+
+            while (_codePointer < _code.Length)
+            {
+                switch (_code[_codePointer])
+                {
+                    case '>':
+                        MoveCursorRight();
+                        break;
+                    case '<':
+                        MoveCursorLeft();
+                        break;
+                    case '+':
+                        Increment();
+                        break;
+                    case '-':
+                        Decrement();
+                        break;
+                    case '[':
+                        ProcessOpeningBracket();
+                        break;
+                    case ']':
+                        ProcessClosingBracket();
+                        break;
+                    case ',':
+                        GetInput();
+                        break;
+                    case '.':
+                        PrintOutput();
+                        break;
+                }
+                _codePointer++;
+            }
+
+            return new string(UTF8Encoding.UTF8.GetChars(_output.ToArray()));
+        }
+
+        private void MoveCursorRight()
+        {
+            if (_dataPointer == _data.Count - 1)
+                _data.Add(0);
+            _dataPointer++;
+        }
+
+        private void MoveCursorLeft()
+        {
+            if (_dataPointer == 0)
+                throw new ApplicationException("Moved pointer before the beggining of the data");
+            else
+                _dataPointer--;
+        }
+
+        private void Increment()
+        {
+            _data[_dataPointer] += 1;
+        }
+
+        private void Decrement()
+        {
+            _data[_dataPointer] -= 1;
+        }
+
+        private void ProcessOpeningBracket()
+        {
+            _lastOpeningBracket = _codePointer;
+            if (_data[_dataPointer] == 0)
+            {
+                while (_codePointer < _code.Length && _code[_codePointer] != ']')
+                    _codePointer++;
+        
+                if (_codePointer == _code.Length)
+                    throw new ApplicationException("No matching closing bracket");
+            }
+        }
+
+        private void ProcessClosingBracket()
+        {
+            if (_data[_dataPointer] == 0)
+            { /*Do nothing*/ }
+            else if (_lastOpeningBracket >= 0)
+                _codePointer = _lastOpeningBracket;
+            else
+                throw new ApplicationException("No matching opening bracket");
+        }
+
+        private void GetInput()
+        {
+            //TODO
+        }
+
+        private void PrintOutput()
+        {
+            _output.Add(_data[_dataPointer]);
+        }
+    }
+}
--- BFPlayground/MainWindow.xaml.cs
+++ BFPlayground/MainWindow.xaml.cs
@@ -27,107 +27,15 @@ namespace BFPlayground
 
         private void GoButton_Click(object sender, RoutedEventArgs e)
         {
+            var interpreter = new BrainFuckInterpreter(this.CodeTextBox.Text);
             try
             {
-                MessageBox.Show(this, ExecuteBF(this.CodeTextBox.Text), "Output");
+                MessageBox.Show(this, interpreter.Run(), "Output");
             }
             catch
             {
                 MessageBox.Show(this, "ERROR", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-        }
-
-        private string ExecuteBF(string program)
-        {
-            var instructions = "<>+-[].,";
-            var code = program.Where(c => instructions.Contains(c)).ToArray();
-            var codePointer = 0;
-            var lastOpeningBracket = -1;
-
-            var data = new List<byte> { 0 };
-            int dataPointer = 0;
-
-            var output = new List<byte>();
-
-            Action MoveCursorRight = () =>
-            {
-                if (dataPointer == data.Count - 1)
-                    data.Add(0);
-                dataPointer++;
-            };
-
-            Action MoveCursorLeft = () =>
-            {
-                if (dataPointer == 0)
-                    throw new ApplicationException("Moved pointer before the beggining of the data");
-                else
-                    dataPointer--;
-            };
-
-            Action Increment = () => data[dataPointer] += 1;
-
-            Action Decrement = () => data[dataPointer] -= 1;
-
-            Action OpeningBracket = () =>
-            {
-                lastOpeningBracket = codePointer;
-                if (data[dataPointer] == 0)
-                {
-                    while (codePointer < code.Length && code[codePointer] != ']')
-                        codePointer++;
-                    if (codePointer == code.Length)
-                    {
-                        throw new ApplicationException("No matching closing bracket");
-                    }
-                }
-            };
-
-            Action ClosingBracket = () =>
-            {
-                if (data[dataPointer] == 0)
-                { /*Do nothing*/ }
-                else if (lastOpeningBracket >= 0)
-                    codePointer = lastOpeningBracket;
-                else
-                    throw new ApplicationException("No matching opening bracket");
-            };
-
-            Action Input = () => { };
-            Action Output = () => output.Add(data[dataPointer]);
-
-            while (codePointer < code.Length)
-            {
-                switch (code[codePointer])
-                {
-                    case '>':
-                        MoveCursorRight();
-                        break;
-                    case '<':
-                        MoveCursorLeft();
-                        break;
-                    case '+':
-                        Increment();
-                        break;
-                    case '-':
-                        Decrement();
-                        break;
-                    case '[':
-                        OpeningBracket();
-                        break;
-                    case ']':
-                        ClosingBracket();
-                        break;
-                    case ',':
-                        Input();
-                        break;
-                    case '.':
-                        Output();
-                        break;
-                }
-                codePointer++;
-            }
-
-            return new string(UTF8Encoding.UTF8.GetChars(output.ToArray()));
         }
     }
 }
